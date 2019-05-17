d3dfe24b14cd883aacd64e530acf5b231d9bf840
Breno <brenord@hotmail.com.br>
2019-05-16 22:53:04 -0300

    [ADD] Hitting Notes

---
+++ Projetogrupo3tentandodnv/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    private SpriteRenderer theSR;
+    public Sprite defaultImg, pressedImg;
+    public KeyCode keyToPress;
+    void Start()
+    {
+        theSR = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(keyToPress))
+        {
+            theSR.sprite = pressedImg;
+        }
+
+        if(Input.GetKeyUp(keyToPress))
+        {
+            theSR.sprite = defaultImg;
+        }
+    }
+}
