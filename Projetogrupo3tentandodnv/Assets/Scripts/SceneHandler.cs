5bd25975b83fe6f7cc1fd00b888dd9c499eb2684
Breno <brenord@hotmail.com.br>
2019-05-23 21:58:58 -0300

    [ADD] Cena de escolha de musica

---
+++ Projetogrupo3tentandodnv/Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void SceneOneChosen ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void SceneTwoChosen ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void SceneThreeChosen ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+}
